3b81ed996920ba59d4737d6a5053d4b0b40509a3
mihyeon <2_5_3_1@naver.com>
2022-03-24 11:18:08 +0900

    search event update

--- src/core/hooks/sortData.js
+++ src/core/hooks/sortData.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 export function useSort() {
   const [sortForm, setSortForm] = useState("");
   const sort = (data) => {
-    let sortData = data;
+    let sortData = [...data];
 
     // 회원(user)
     if (sortForm === "userName") {
@@ -18,19 +18,18 @@ export function useSort() {
     }
     if (sortForm === "date") {
     }
+
+    // 펀드(fund)
     if (sortForm === "fundName") {
       sortData.sort(function (a, b) {
         return a.fundName < b.fundName ? -1 : a.fundName > b.fundName ? 1 : 0;
       });
     }
     if (sortForm === "fundTotalCost") {
-      console.log("fundTotalCost");
-
       sortData.sort(function (a, b) {
-        return Number(a.fundTotalCost) - Number(b.fundTotalCost);
+        return Number(b.fundTotalCost) - Number(a.fundTotalCost);
       });
     }
-    // 회원(fund)
 
     return sortData;
   };
